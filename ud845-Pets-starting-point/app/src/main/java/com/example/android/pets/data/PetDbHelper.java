00940ff369ab56cdd947ae9c79519c47ee61d837
Naman Kaushik <kaushiknaman.dk@gmail.com>
2022-05-24 03:47:40 +0530

    pet db app

---
+++ ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,44 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.pets.CatalogActivity;
+import com.example.android.pets.data.PetsContract.PetsEntry;
+
+import androidx.annotation.Nullable;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+    public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "shelter.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + PetsEntry.table_name + " ("
+                + PetsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetsEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetsEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetsEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetsEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
